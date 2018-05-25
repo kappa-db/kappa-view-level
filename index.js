7c366c1b5f73a25e2f243ab9fc0e12b1762c7545
noffle <sww@eight45.net>
2018-05-24 19:02:28 -0700

    impl

---
+++ index.js
@@ -0,0 +1,39 @@
+module.exports = createIndex
+
+function createIndex (ldb, opts) {
+  return {
+    maxBatch: 100 || opts.maxBatch,
+
+    map: function (msgs, next) {//opts.map,
+      var ops = []
+      msgs.forEach(function (msg) {
+        var res = opts.map(msg)
+        if (res && Array.isArray(res)) {
+          res = res.map(function (r) {
+            return {
+              type: 'put',
+              key: r[0],
+              value: r[1]
+            }
+          })
+          ops.push.apply(ops, res)
+        }
+      })
+      ldb.batch(ops, next)
+    },
+
+    api: opts.api,
+
+    storeState: function (state, cb) {
+      ldb.put('state', state, cb)
+    },
+
+    fetchState: function (cb) {
+      ldb.get('state', function (err, state) {
+        if (err && err.notFound) cb()
+        else if (err) cb(err)
+        else cb(null, state)
+      })
+    }
+  }
+}
