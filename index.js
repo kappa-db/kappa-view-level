635d6ae8be41d391fd0aac131d6febef1d605200
noffle <sww@eight45.net>
2019-11-17 20:13:11 -0800

    fix: use kappa-view -> store 'state' separately
    Fixes https://github.com/noffle/kappa-view-level/issues/2
    
    This will also cause index regeneration for all downstreamers who
    upgrade, since 'state' will now be stored in a separate location. Since
    data is also now stored in a sublevel, it should appear to the
    downstream user like the database is empty anew.
    
    Unfortunately it also means the old non-sublevel'd data will sit in the
    leveldb indefinitely. I don't think many folx are using this module yet
    for Super Important Data, so this should be okay.

--- index.js
+++ index.js
@@ -1,69 +1,33 @@
-var Writable = require('readable-stream').Writable
-var pump = require('pump')
+var View = require('kappa-view')
 
 module.exports = createIndex
 
 function createIndex (ldb, opts) {
-  return {
-    maxBatch: 100 || opts.maxBatch,
+  return View(ldb, opts, function (db) {
+    return {
+      map: function (msgs, next) {
+        var ops = []
+        msgs.forEach(function (msg) {
+          var res = opts.map(msg)
+          if (res && Array.isArray(res)) {
+            res = res.map(function (r) {
+              return {
+                type: 'put',
+                key: r[0],
+                value: r[1]
+              }
+            })
+            ops.push.apply(ops, res)
+          }
+        })
+        ldb.batch(ops, next)
+      },
 
-    map: function (msgs, next) {//opts.map,
-      var ops = []
-      msgs.forEach(function (msg) {
-        var res = opts.map(msg)
-        if (res && Array.isArray(res)) {
-          res = res.map(function (r) {
-            return {
-              type: 'put',
-              key: r[0],
-              value: r[1]
-            }
-          })
-          ops.push.apply(ops, res)
-        }
-      })
-      ldb.batch(ops, next)
-    },
+      api: opts.api,
 
-    api: opts.api,
-
-    indexed: function (msgs) {
-      if (opts.indexed) opts.indexed(msgs)
-    },
-
-    storeState: function (state, cb) {
-      state = state.toString('base64')
-      ldb.put('state', state, cb)
-    },
-
-    fetchState: function (cb) {
-      ldb.get('state', function (err, state) {
-        if (err && err.notFound) cb()
-        else if (err) cb(err)
-        else cb(null, Buffer.from(state, 'base64'))
-      })
-    },
-
-    clearIndex: opts.clearIndex || function (cb) {
-      var batch = []
-      var maxSize = 5000
-      pump(ldb.createKeyStream(), new Writable({
-        objectMode: true,
-        write: function (key, enc, next) {
-          batch.push({ type: 'del', key })
-          if (batch.length >= maxSize) {
-            ldb.batch(batch, next)
-          } else next()
-        },
-        final: function (next) {
-          if (batch.length > 0) ldb.batch(batch, next)
-          else next()
-        }
-      }), ondone)
-      function ondone (err) {
-        if (err) cb(err)
-        else cb()
+      indexed: function (msgs) {
+        if (opts.indexed) opts.indexed(msgs)
       }
     }
-  }
+  })
 }
